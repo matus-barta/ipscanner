5507f5fbc3996b717123a638ed02452f1ab1cbcf
Matus Barta <matus.barta64@gmail.com>
2025-09-18 18:12:13 +0200

    Initial Commit

---
+++ ipscanner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ipscanner
+//
+//  Created by Matúš Barta on 18/09/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ipscanner/ipscannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ipscannerApp.swift
+//  ipscanner
+//
+//  Created by Matúš Barta on 18/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct ipscannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
